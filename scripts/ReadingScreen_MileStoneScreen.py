fe3fee2547a75162fc4e7ed6c63ee126f6d4dd8f
harwooja1008 <jacobharwood0@gmail.com>
2023-11-01 12:16:23 -0400

    Removed TTS Player from Milestone Screen

--- scripts/ReadingScreen_MileStoneScreen.py
+++ scripts/ReadingScreen_MileStoneScreen.py
@@ -299,6 +299,10 @@ class Ui_ReadingScreen(QtWidgets.QMainWindow):
         """Load generic milestone screen."""
         self.mileStoneScreen = mileStoneScreen.Ui_Generic_Milestone(self.gridLayout, self)
         self.textBrowser.hide()
+        self.muted = False
+        self.toggleTTS()
+        self.backgroundFrame.hide()
+        self.textToSpeech.hide()
         self.gridLayout.update()
 
     def loadTextBrowser(self):
